c3d163521ca7cd438417ad94297dabae3da70968
anhtuan <anhtuan997tk@gmail.com>
2025-04-04 01:21:13 +0900

    first commit

---
+++ bank_account.py
@@ -0,0 +1,23 @@
+class BankAccount:
+    def __init__(self, balance=0):
+        self.__balance = balance
+    def deposit(self, amount):
+        self.__balance += amount
+    def withdraw(self, amount):
+        if 0 < amount <= self.__balance:
+            self.__balance -= amount
+        else:
+            print("So tien khong hop le")
+
+    @property
+    def balance(self):
+        return self.balance
+    def __str__(self):
+        return f"so du: {self.__balance}"
+
+#test
+if __name__ == "__main__":
+    account = BankAccount(1000)
+    account.deposit(500)
+    account.withdraw(200)
+    print(account)
